0cbc331e6576f5bd3e73f639a9a4b2d30414c6c9
ajanmorina57-ux <ajanmorina57@gmail.com>
2025-11-11 22:30:18 +0100

    Change server port from 4000 to 3000

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Bot is alive!'));
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`✅ Keep-alive server running on port ${port}`);
